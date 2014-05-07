fb6312a03dac2f0442ac316c751640b3abf24721
David Keijser <david.keijser@klarna.com>
2014-05-07 13:24:29 +0200

    a basic list of packages

---
+++ billom.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var vm = require('npm-check-updates/lib/versionmanager'),
+    async = require('async'),
+    hogan = require('hogan.js'),
+    tmpl;
+
+tmpl = "<!doctype html><html><head>" +
+    "<style>body {font-size: 16px;} tr.outdated {background: red}</style>" +
+    "</head><body><table>" +
+    "<tr><td>package</td><td>current</td><td>installed</td><td>latest</td></tr>" +
+    "{{#packages}}<tr class=\"package{{#outdated}} outdated{{/outdated}}\">" +
+    "<td>{{package}}</td><td>{{current}}</td><td>{{installed}}</td><td>{{latest}}</td></tr>{{/packages}}" +
+    "</table></body></html>";
+
+vm.initialize(false, function () {
+    var packageFile = 'package.json',
+        template = hogan.compile(tmpl);
+
+    async.series({
+        current: function (callback) {
+            vm.getCurrentDependencies(packageFile, callback);
+        },
+        installed: function (callback) {
+            vm.getInstalledPackages(callback);
+        }
+    }, function (err, results) {
+        var packages;
+
+        if (err) {
+            return console.error('An error occured: ' + err);
+        }
+
+        packages = Object.keys(results.installed);
+
+        vm.getLatestVersions(packages, function (err, latest) {
+            var result;
+
+            result = packages.map(function (key) {
+                var lv = latest[key],
+                    iv = results.installed[key],
+                    cv = results.current[key];
+
+                return {
+                    package: key,
+                    current: cv,
+                    installed: iv,
+                    latest: lv,
+                    outdated: iv !== lv
+                };
+            });
+
+            console.log(template.render({packages: result}));
+        });
+    });
+});
