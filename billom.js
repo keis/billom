c043323790acc5a331627518423742119005ba3b
David Keijser <david.keijser@klarna.com>
2015-06-02 17:37:01 +0200

    lint code

--- billom.js
+++ billom.js
@@ -1,118 +1,119 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-var dependencyDetails = require('npm-dependency-details'),
-    parallel = require('run-parallel'),
-    npmconf = require('npmconf'),
-    hogan = require('hogan.js'),
-    path = require('path'),
-    fs = require('fs'),
-    rapidus = require('rapidus'),
-    createFormatter = require('rapidus-sparkle').createFormatter,
-    npmLogger = require('./npm-logger');
+var dependencyDetails = require('npm-dependency-details')
+  , parallel = require('run-parallel')
+  , npmconf = require('npmconf')
+  , hogan = require('hogan.js')
+  , path = require('path')
+  , fs = require('fs')
+  , rapidus = require('rapidus')
+  , createFormatter = require('rapidus-sparkle').createFormatter
+  , npmLogger = require('./npm-logger')
 
 // Load a mustache template
 function loadTemplate(filename, callback) {
-    fs.readFile(filename, 'utf-8', function (err, data) {
-        var template;
+  fs.readFile(filename, 'utf-8', function (err, data) {
+    var template
 
-        if (err) {
-            return callback(err);
-        }
-
-        try {
-            template = hogan.compile(data);
-        } catch (err) {
-            return callback(err);
-        }
-
-        callback(null, template);
-    });
-
-    return function (fun) {
-        callback = fun;
+    if (err) {
+      return callback(err)
     }
+
+    try {
+      template = hogan.compile(data)
+    } catch (err) {
+      return callback(err)
+    }
+
+    callback(null, template)
+  })
+
+  return function (fun) {
+    callback = fun
+  }
 }
 
 // Configure npm
 function configure(callback) {
-    npmconf.load({log: npmLogger}, callback);
+  npmconf.load({log: npmLogger}, callback)
 }
 
 // Generate report on used packages
 function generate(config, dir, title, callback) {
-    parallel([
-        loadTemplate(path.join(__dirname, 'template.tmpl')),
-        dependencyDetails({config: config, depth: 0}, dir)
-    ], function (err, results) {
-        var packages,
-            details,
-            template,
-            result;
+  parallel([ loadTemplate(path.join(__dirname, 'template.tmpl'))
+           , dependencyDetails({config: config, depth: 0}, dir)
+           ]
+           , function (err, results) {
+    var packages
+      , details
+      , template
+      , result
 
-        if (err) {
-            callback(err);
-        }
+    if (err) {
+      callback(err)
+    }
 
-        template = results[0];
-        details = results[1];
-        packages = Object.keys(details);
+    template = results[0]
+    details = results[1]
+    packages = Object.keys(details)
 
-        result = packages.map(function (key) {
-            var detail = details[key];
+    result = packages.map(function (key) {
+      var detail = details[key]
 
-            return {
-                package: key,
-                current: detail.current,
-                installed: detail.installed,
-                latest: detail.latest,
-                outdated: detail.installed !== detail.latest
-            };
-        });
+      return { package: key
+             , current: detail.current
+             , installed: detail.installed
+             , latest: detail.latest
+             , outdated: detail.installed !== detail.latest }
+    })
 
-        callback(null, template.render({
-            title: title,
-            packages: result
-        }));
-    });
+    callback(null, template.render({ title: title
+                                   , packages: result
+                                   }))
+  })
 }
 
 function main() {
-    var yargs = require('yargs'),
-        argv,
-        packageFile,
-        title,
-        dir;
+  var yargs = require('yargs')
+    , argv
+    , packageFile
+    , title
+    , dir
 
-    argv = yargs
-        .usage('Usage: $0 [package.json] [title]')
-        .argv
+  argv = yargs
+      .usage('Usage: $0 [package.json] [title]')
+      .argv
 
-    if (argv.help) {
-        return console.log(yargs.help());
+  if (argv.help) {
+    return console.log(yargs.help())
+  }
+
+  packageFile = argv._[0] || 'package.json'
+  title = argv._[1] || false
+  dir = path.dirname(packageFile)
+
+  rapidus.getLogger('attempt').setLevel('ERROR')
+  rapidus.getLogger().addSink(rapidus.sinks.console({
+      format: createFormatter('%{green [:date :time]} :name [:levelName] - :message')
+  }))
+
+  configure(function (err, config) {
+    if (err) {
+      return console.error('An error occured: ' + err)
     }
 
-    packageFile = argv._[0] || 'package.json';
-    title = argv._[1] || false;
-    dir = path.dirname(packageFile);
+    generate(config, dir, title, function (err, result) {
+      if (err) {
+        return console.error('An error occured: ' + err)
+      }
 
-    rapidus.getLogger('attempt').setLevel('ERROR');
-    rapidus.getLogger().addSink(rapidus.sinks.console({
-        format: createFormatter('%{green [:date :time]} :name [:levelName] - :message')
-    }));
-
-    configure(function (err, config) {
-        generate(config, dir, title, function (err, result) {
-            if (err) {
-                return console.error('An error occured: ' + err);
-            }
-
-            process.stdout.write(result);
-        });
-    });
+      process.stdout.write(result)
+    })
+  })
 }
 
 // Do stuff if this is the main module.
 if (require.main === module) {
-    main();
+  main()
 }
--- npm-logger.js
+++ npm-logger.js
@@ -1,29 +1,32 @@
-var rapidus = require('rapidus');
+var rapidus = require('rapidus')
 
 function logFunction(level) {
-    return function () {
-        var cat = arguments[0],
-            args = Array.prototype.slice.call(arguments, 1),
-            logger = rapidus.getLogger(cat);
+  return function () {
+    var cat = arguments[0]
+      , args = Array.prototype.slice.call(arguments, 1)
+      , logger = rapidus.getLogger(cat)
 
-        args.unshift(level);
-        logger.log.apply(logger, args);
-    };
+    args.unshift(level)
+    logger.log.apply(logger, args)
+  }
 }
 
-module.exports = {
-    silly: logFunction(1),
-    verbose: logFunction('DEBUG'),
-    info: logFunction('INFO'),
-    warn: logFunction('WARN'),
-    error: logFunction('ERROR'),
-    silent: logFunction(0),
-    http: function () {
-        var logger = rapidus.getLogger('http'),
-            req = arguments[0] === 'request';
+function logHTTP() {
+  var logger = rapidus.getLogger('http')
+    , req = arguments[0] === 'request'
 
-        logger.info('%s %s',
-                    req ? arguments[1] : arguments[0],
-                    req ? arguments[2] : arguments[1]);
-    }
-};
+  logger.info( '%s %s'
+             , req ? arguments[1] : arguments[0]
+             , req ? arguments[2] : arguments[1]
+             )
+}
+
+module.exports =
+  { silly: logFunction(1)
+  , verbose: logFunction('DEBUG')
+  , info: logFunction('INFO')
+  , warn: logFunction('WARN')
+  , error: logFunction('ERROR')
+  , silent: logFunction(0)
+  , http: logHTTP
+  }
